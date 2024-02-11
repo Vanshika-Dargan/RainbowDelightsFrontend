164ed60e69550c00af595181c3e745a4eb555626
TirthajyotiNag <tirthajyotinag.2001@gmail.com>
2024-02-11 23:19:11 +0530

    Navbar Updated - Tirthajyoti Nag.

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -1,21 +1,43 @@
-import "./Header.css"
-import User from "../../assets/user.png";
-import Cart from "../../assets/Cart.png"
-export default function Header()
-{
-    return(
-        <div className="header">
-            <div className="name">
-               Rainbow Delights
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import UserIcon from '../../assets/user.png';
+import CartIcon from '../../assets/Cart.png';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <nav className="bg-white shadow-md">
+            <div className="max-w-7xl mx-auto px-4">
+                <div className="flex justify-between items-center py-3">
+                    {/* Website Logo */}
+                    <a href="#" className="flex items-center">
+                        <span className="text-gray-500 text-5xl cursor-pointer logo-font">Rainbow Delights</span>
+                    </a>
+
+                    {/* Search Input */}
+                    <div className="flex justify-center items-center relative">
+                        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                            <FaSearch className="text-gray-500" />
+                        </div>
+                        <input
+                            className="border-2 border-gray-300 bg-white h-10 pl-10 pr-16 rounded-lg text-sm focus:outline-none w-full"
+                            type="search"
+                            name="search"
+                            placeholder="Find your favorite delight..."
+                        />
+                    </div>
+
+                    {/* Cart and User Icons */}
+                    <div className="flex items-center space-x-4">
+                        <a href="#" className="flex items-center">
+                            <img src={UserIcon} alt="User" className="h-6 w-6" />
+                        </a>
+                        <a href="#" className="flex items-center">
+                            <img src={CartIcon} alt="Cart" className="h-6 w-6" />
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div>
-              
-              <input className="nosubmit"  placeholder="Find your favorite delight.."/>
-            </div>
-            <div className="flex">
-                <a href="#" ><img src={User}></img></a>
-                <a href="#"><img src={Cart}></img></a>
-            </div>
-        </div>
-    )
+        </nav>
+    );
 }
