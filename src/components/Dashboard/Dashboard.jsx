a6d34568f71d7adf9821e26049bfb6c1f33a8f69
Aalishan17Raza <aalishan69raza@gmail.com>
2024-02-15 19:31:15 +0530

    improved dropdown

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,27 +2,46 @@ import React, { useState } from 'react'
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
+import { useRef } from 'react';
 
 export default function Dashboard() {
     const [isDashboardClicked, setIsDashboardClicked] = useState(false);
-    
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+
     return (
         <div className='flex'>
             <div className='w-1/4 bg-color-1'>
                 <div className='logo-font'>Rainbow delights</div>
                 <div className='color-1 mt-10'>MENU</div>
-                <div className='flex das ml-5 mt-2' onClick={()=>{setIsDashboardClicked(prev=> !prev)}}>
+                <div className='flex das ml-5 mt-2' onClick={() => { setIsDashboardClicked(prev => !prev) }}>
                     <AiOutlineDashboard className='das-icon-1 ml-5 mr-2 mt-1' />
                     Dashboard
                     {isDashboardClicked ?
-                            <IoIosArrowUp className='mt-2 ml-auto mr-4 down-up-icon'/>
-                        :   <IoIosArrowDown className='mt-2 ml-auto mr-4 down-up-icon' />
+                        <IoIosArrowUp className='mt-2 ml-auto mr-4 down-up-icon' />
+                        : <IoIosArrowDown className='mt-2 ml-auto mr-4 down-up-icon' />
                     }
                 </div>
                 {isDashboardClicked &&
                     <div className='dropdown-items flex flex-col'>
-                        <span className='dropdown-item ml-24'>Default</span>
-                        <span className='dropdown-item ml-24'>Products</span>
+                        <span
+                            ref={inputRef1}
+                            onClick={() => {
+                                inputRef1.current.style.color = 'white';
+                                inputRef2.current.style.color = '#6A7187';
+                            }}
+                            className='dropdown-item ml-24 mt-1'>
+                            Default
+                        </span>
+                        <span
+                            ref={inputRef2}
+                            onClick={() => {
+                                inputRef2.current.style.color = 'white';
+                                inputRef1.current.style.color = '#6A7187';
+                            }}
+                            className='dropdown-item ml-24'>
+                            Products
+                        </span>
                     </div>
                 }
             </div>
