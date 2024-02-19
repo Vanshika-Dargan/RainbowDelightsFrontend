523a7a6383a7e4e6af1592ebbf582607241ef0fc
TirthajyotiNag <tirthajyotinag.2001@gmail.com>
2024-02-19 11:00:12 +0530

    Adding Shopping Cart Module

---
+++ src/CartProductsData.js
@@ -0,0 +1,156 @@
+import Cookies from "./assets/Products/cookies.jpg";
+import BCake from "./assets/Products/bithdaycake.jpg"
+import ChocoCake from "./assets/Products/choco_cake.jpg";
+import ChoCookie from "./assets/Products/choco_cookie.jpg";
+import IceCake from "./assets/Products/icecream_cake.jpg";
+import Pie from "./assets/Products/pie.jpg";
+import CupCake from "./assets/Products/cupcakes.jpg";
+import Eclairs from "./assets/Products/eclairs.jpg";
+import butter_cookie from "./assets/Products/butter_cookies.jpg";
+import cheese_bread from "./assets/Products/cheese_bread.jpg";
+import choco_balls from "./assets/Products/choco_balls.jpg";
+import chocolate from "./assets/Products/chocolate.jpg";
+import fruit_bread from "./assets/Products/fruit_bread.jpg";
+import garlic_bread from "./assets/Products/garlic_bread.jpg";
+import jam_cookies from "./assets/Products/jam_cookies.jpg";
+import rainbow_cookies from "./assets/Products/rainbow_cookies.jpg";
+import white_chocolate from "./assets/Products/white_choco.jpg";
+const ProductsData=[
+    {
+        id: 1,
+        name: "Birthday Cakes",
+        img: BCake,
+        price: 100,
+        piece:null,
+        quantity: 2
+    },
+    {
+        id: 2,
+        name: "Chocolate Cakes",
+        img: ChocoCake,
+        price: 75,
+        piece:3,
+        quantity: 2
+    },
+    {
+        id: 3,
+        name: "Chocolate Cookies",
+        img: ChoCookie,
+        price: 100,
+        piece:5,
+        quantity: 2
+    },
+    {
+        id: 4,
+        name: "Cookies",
+        img: Cookies,
+        price: 20,
+        piece:5,
+        quantity: 2
+    },
+    {
+        id: 5,
+        name: "Icecream Cakes",
+        img: IceCake,
+        price: 200,
+        piece:2,
+        quantity: 2
+    },
+    {
+        id: 6,
+        name: "Eclairs",
+        img: Eclairs,
+        price: 200,
+        piece:7,
+        quantity: 2
+    },
+    {
+        id: 7,
+        name: "Cup Cakes",
+        img: CupCake,
+        price: 200,
+        piece:10,
+        quantity: 2
+    },
+    {
+        id: 8,
+        name: "Pie",
+        img: Pie,
+        price: 200,
+        piece:null,
+        quantity: 2
+    },
+    {
+        id: 9,
+        name: "Butter Cookies",
+        img: butter_cookie,
+        price: 100,
+        piece:5,
+        quantity: 2
+    },
+    {
+        id: 10,
+        name: "Cheese Bread",
+        img: cheese_bread,
+        price: 300,
+        piece:null,
+        quantity: 2
+    },
+    {
+        id: 11,
+        name: "Choco Balls",
+        img: choco_balls,
+        price: 50,
+        piece:5,
+        quantity: 2
+    },
+    {
+        id: 12,
+        name: "Chocolates",
+        img: chocolate,
+        price: 50,
+        piece:5,
+        quantity: 2
+    },
+    {
+        id: 13,
+        name: "Fruit Bread",
+        img: fruit_bread,
+        price: 200,
+        piece:null,
+        quantity: 2
+    },
+    {
+        id: 14,
+        name: "Garlic Bread",
+        img: garlic_bread,
+        price: 200,
+        piece:null,
+        quantity: 2
+    },
+    {
+        id: 15,
+        name: "Jam Cookies",
+        img: jam_cookies,
+        price:75,
+        piece:7,
+        quantity: 2
+    },
+    {
+        id: 16,
+        name: "Rainbow Cookies",
+        img: rainbow_cookies,
+        price: 100,
+        piece:5,
+        quantity: 2
+    },
+    {
+        id: 17,
+        name: "White Chocolates",
+        img: white_chocolate,
+        price: 150,
+        piece:3,
+        quantity: 2
+    }
+]
+export default ProductsData;
--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+// ShoppingCart.jsx
+import React from 'react';
+import CartCard from './CartCard/CartCard';
+import ProductsData from '../../CartProductsData';
+import './ShoppingCart.css';
 
-const ShoppingCart = ({cart}) => {
+const ShoppingCart = () => {
   return (
-    <div>ShoppingCart</div>
-    
-  )
-}
+    <div className="container mx-auto p-6">
+      <div className="text-5xl font-medium mb-4 text-color">Your Cart</div>
+      {ProductsData.map(product => (
+        <CartCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
 
-export default ShoppingCart
+export default ShoppingCart;
