3db4f0cb32403d932d8d2b9eafdb9fdb51ae42e2
Aalishan17Raza <aalishan69raza@gmail.com>
2024-02-26 21:48:00 +0530

    fetched products from database

--- src/components/Dashboard/Product/EachProduct.jsx
+++ src/components/Dashboard/Product/EachProduct.jsx
@@ -7,6 +7,9 @@ export const EachProduct = ({ productData, setIsProductEdited ,updateCurrentEdit
             <td className=''><img className='w-full h-32 object-cover' src={productData.img} alt={productData.name} /></td>
             <td className=''>{productData.name}</td>
             <td className=''>{productData.price}</td>
+            <td className=''>{productData.weight}</td>
+            <td className=''>{productData.netQuantity}</td>
+            <td className=''>{productData.category}</td>
             <td className='w-72'>{productData.description}</td>
             <td className=''>
                 <span className='text-cyan-800 cursor-pointer' onClick={() => { setIsProductEdited(true); updateCurrentEditProduct(productData); }}>Edit</span>&nbsp;&nbsp;&nbsp;<span className='text-red-700 cursor-pointer'>Delete</span></td>
--- src/components/Dashboard/Product/Product.jsx
+++ src/components/Dashboard/Product/Product.jsx
@@ -1,25 +1,23 @@
-import React, {useEffect, useState} from 'react';
-import productData from '../../../ProductsData';
+import React, { useEffect, useState } from 'react';
+// import productData from '../../../ProductsData';
 import { EachProduct } from './EachProduct';
 
 // Destructure setIsProductEdited from props
-export const Product = ({ setIsProductEdited, updateCurrentEditProduct ,setIsAddProductClicked }) => {
-
-
-    const [data, setData] = useState(null);
+export const Product = ({ setIsProductEdited, updateCurrentEditProduct, setIsAddProductClicked }) => {
+    const [productData, setproductData] = useState([]);
+    
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/product/products');
+            const jsonData = await response.json();
+            setproductData(jsonData);
+            console.log(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:1000/product/products');
-                const jsonData = await response.json();
-                setData(jsonData);
-                console.log(jsonData);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
         fetchData();
     }, []);
 
@@ -32,7 +30,7 @@ export const Product = ({ setIsProductEdited, updateCurrentEditProduct ,setIsAdd
             <div className='ml-2 py-2 text-2xl'>All Products</div>
             <div className='flex justify-between'>
                 <input className='searchbox p-2 w-4/12 ml-2 my-3' type='search' placeholder='search for products' />
-                <button className='bg-cyan-600 hover:bg-cyan-700 mr-3 h-fit p-2 rounded-md text-white m-3' onClick={()=>{setIsAddProductClicked(true)}}>Add New Product</button>
+                <button className='bg-cyan-600 hover:bg-cyan-700 mr-3 h-fit p-2 rounded-md text-white m-3' onClick={() => { setIsAddProductClicked(true) }}>Add New Product</button>
             </div>
             <div className='product-table'>
                 <table className='text-gray-950 w-full'>
@@ -41,12 +39,11 @@ export const Product = ({ setIsProductEdited, updateCurrentEditProduct ,setIsAdd
                             <th className='py-3 text-sm'>PRODUCT ID</th>
                             <th className='py-3 text-sm'>IMAGE</th>
                             <th className='py-3 text-sm'>NAME</th>
-                            <th className='py-3 text-sm'>COST PER ITEM</th>
+                            <th className='py-3 text-sm'>PRICE</th>
                             <th className='py-3 text-sm'>WEIGHT PER ITEM</th>
-                            <th className='py-3 text-sm'>QUANTITY PER ITEM</th>
                             <th className='py-3 text-sm'>NET QUANTITY</th>
                             <th className='py-3 text-sm'>CATEGORY</th>
-                            <th className='py-3 text-sm'>INGREDIENTS</th>
+                            <th className='py-3 text-sm'>DESCRIPTION</th>
 
 
                             <th className='py-3 text-sm'>ACTION</th>
