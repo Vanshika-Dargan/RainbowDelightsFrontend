b96f270dbc7082b3b97e3af58e5fc094925ce120
TirthajyotiNag <tirthajyotinag.2001@gmail.com>
2024-03-05 12:19:40 +0530

    Customization page styling

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -14,13 +14,20 @@ import sliderImg from '../../assets/cake2.png';
 export default function App() {
   const slides = [
     {
-      heading: "Lorem ipsum",
-      subHeading1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque suscipit illum aut maxime ut!"
+      heading: "Our Customers Speak Out!",
+      subHeading1: '"Absolutely divine! Every bite of their pastries is like a taste of heaven. The attention to detail and freshness in their products really sets them apart. Highly recommended!"',
+      name:"Emily"
     },
     {
-      heading: 'dolor sit.',
-      subHeading1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque suscipit illum aut maxime ut!"
+      heading: 'Our Customers Speak Out!',
+      subHeading1: '"Hands down the best bakery in town! I\'ve tried many others, but none come close to the quality and flavor they offer here."',
+      name:'Mark'
     },
+    {
+      heading: 'Our Customers Speak Out!',
+      subHeading1: '"The attention to detail in their desserts is unmatched. You can taste the passion in every bite!" ',
+      name:'Michael L'
+    }
     // Add more slide data here
   ];
 
@@ -32,15 +39,16 @@ export default function App() {
     >
       {slides.map((slide, index) => (
         <SwiperSlide key={index}>
-          <div className="slide-background">
-            <img src={sliderImg} alt={`Slide ${index + 1}`} className="slide-image" />
-            <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-20"></div>
-            <div className="slide-text">
-              <h2 className="slide-heading text-4xl mb-5 font-extrabold">{slide.heading}</h2>
-              <h3 className="slide-subheading text-3xl leading-10 font-medium">{slide.subHeading1}</h3>
-            </div>
+        <div className="slide-background">
+          <img src={sliderImg} alt={`Slide ${index + 1}`} className="slide-image" />
+          <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-20"></div>
+          <div className="slide-text">
+            <h2 className="slide-heading text-4xl mb-5 font-extrabold">{slide.heading}</h2>
+            <h3 className="slide-subheading text-3xl leading-10 font-medium">{slide.subHeading1}</h3>
+            <h3 className='text-right'>-{slide.name}</h3>
           </div>
-        </SwiperSlide>
+        </div>
+      </SwiperSlide>
       ))}
     </Swiper>
   );
